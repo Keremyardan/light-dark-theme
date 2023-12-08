4c50e95f82d90c2a5e9a791a147337f66f11928a
unknown <keremyrdn@hotmail.com>
2023-12-08 20:41:15 +0300

    light and dark theme created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeProvider, useTheme } from "./ThemeContext";
+import Switch from "./Switch";
+
+const Title = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <h2
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </h2>
+  );
+};
+
+const Paragraph = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <p
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </p>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Maecenas sodales molestie erat non facilisis. 
+      Fusce est urna, accumsan at sagittis eu, blandit sed lorem. Cras sollicitudin, augue id malesuada dictum, 
+      nisl felis tincidunt lectus, eu venenatis sem nisl vel felis. Quisque sed imperdiet enim. 
+      Integer vestibulum efficitur libero, nec ullamcorper nunc fermentum vitae. 
+      Cras eget justo non nunc sodales scelerisque sit amet mollis ipsum. 
+      Nullam eu arcu in magna luctus pretium id eget nulla. Sed eget dapibus libero. 
+      Nullam a mi nisi. Curabitur bibendum laoreet pretium. Aliquam malesuada lectus massa, non pellentesque metus porttitor ac. 
+      Quisque non rutrum ex, eget sagittis enim. Suspendisse ac dui laoreet, fringilla felis ut, tristique purus. 
+      Sed ex magna, iaculis a mollis vitae, auctor eu odio. Etiam eu dignissim ante, ut consectetur urna. Pellentesque euismod mi vel diam consequat euismod.
+      </Paragraph>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <Title>Little Cam! 🎥</Title>
+      <Switch />
+    </header>
+  );
+};
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
+    </div>
+  );
+};
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundColor: theme === "light" ? "white" : "black",
+      }}
+    >
+      <Header />
+      <Page />
     </div>
   );
 }
 
-export default App;
+function Root() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export default Root;
